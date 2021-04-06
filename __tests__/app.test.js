f11704a3577c9684e7cabd73befbeb29cd744885
KB <katrinkajb@gmail.com>
2021-04-06 16:57:00 -0700

    post test written

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -7,4 +7,27 @@ describe('breweries-crud-app routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
+
+  // beforeEach(async () => {
+  //   await 
+  // });
+
+  it('adds a brewery to favorites', () => {
+    const newBrew = {name: 'Breakside Brewery', type: 'micro', city: 'Portland', state: 'Oregon'}
+
+    return request(app)
+      .post('/api.v1/breweries')
+      .send(newBrew)
+      .then((res) => {
+
+        expect(res.body).toEqual(newBrew);
+      })
+  })
+
+  it.skip('gets a list of all favorites', () => {
+
+
+    expect().toEqual();
+  })
+
 });
---
+++ sql/setup.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS favorites;
+
+CREATE TABLE favorites (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name TEXT NOT NULL,
+    type TEXT NOT NULL,
+    city TEXT NOT NULL,
+    state TEXT NOT NULL
+)
