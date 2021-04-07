c08b4fe174c910090f34f56922a7dcc5051e7cdf
KB <katrinkajb@gmail.com>
2021-04-06 18:01:37 -0700

    stuck on post test. will dig into it more later

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const Fave = require('../lib/model/Fave');
 
 describe('breweries-crud-app routes', () => {
   beforeEach(() => {
@@ -9,14 +10,14 @@ describe('breweries-crud-app routes', () => {
   });
 
   // beforeEach(async () => {
-  //   await 
+  //   await Fave.insert({ name: 'Base Camp Brewing', type: 'micro', city: 'Portland', state: 'Oregon' })
   // });
 
   it('adds a brewery to favorites', () => {
-    const newBrew = {name: 'Breakside Brewery', type: 'micro', city: 'Portland', state: 'Oregon'}
+    const newBrew = {id: '2', name: 'Breakside Brewery', type: 'micro', city: 'Portland', state: 'Oregon' };
 
     return request(app)
-      .post('/api.v1/breweries')
+      .post('/api/v1/favorites')
       .send(newBrew)
       .then((res) => {
 
@@ -24,10 +25,5 @@ describe('breweries-crud-app routes', () => {
       })
   })
 
-  it.skip('gets a list of all favorites', () => {
-
-
-    expect().toEqual();
-  })
 
 });
