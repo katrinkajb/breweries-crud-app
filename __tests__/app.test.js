4ad062d7e49c504f58f00745042de53c265d1768
KB <katrinkajb@gmail.com>
2021-04-07 09:36:19 -0700

    get all test written

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,19 +2,19 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
-// const Fave = require('../lib/model/Fave');
+const Fave = require('../lib/model/Fave');
 
 describe('breweries-crud-app routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
 
-  // beforeEach(async () => {
-  //   await Fave.insert({ name: 'Base Camp Brewing', type: 'micro', city: 'Portland', state: 'Oregon' })
-  // });
+  beforeEach(async () => {
+    await Fave.insert({ name: 'Base Camp Brewing', type: 'micro', city: 'Portland', state: 'Oregon' })
+  });
 
   it('adds a brewery to favorites', () => {
-    const newBrew = {id: '1', name: 'Breakside Brewery', type: 'micro', city: 'Portland', state: 'Oregon' };
+    const newBrew = {id: '2', name: 'Breakside Brewery', type: 'micro', city: 'Portland', state: 'Oregon' };
 
     return request(app)
       .post('/api/v1/favorites')
@@ -25,5 +25,14 @@ describe('breweries-crud-app routes', () => {
       })
   })
 
+  it('gets all breweries in favorites', () => {
+    const existingBrew = [{ name: 'Base Camp Brewing', type: 'micro', city: 'Portland', state: 'Oregon' }];
 
+    return request(app)
+      .get('/api/v1/favorites')
+      .then((res) => {
+
+        expect(res.body).toEqual(existingBrew);
+      })
+  })
 });
